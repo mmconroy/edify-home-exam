f8526c54c9a2adeccf892166fd7cfee3a6b0f656
Matthew Conroy <mmconroy@gmail.com>
2020-10-28 19:07:32 -0400

    Header added

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          ></IconButton>
+          <Typography variant="h6">Place something here</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;

2f7970f150f50850dce460a82877a87c36c2856a
Matthew Conroy <mmconroy@gmail.com>
2020-10-29 01:13:27 -0400

    Search component working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 import Header from "./Header";
+import Search from "./Search";
 import List from "./List";
 import ItemDetail from "./ItemDetail";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     fetch("https://5f9a10979d94640016f705ed.mockapi.io/api/vi/users")
@@ -14,10 +17,22 @@ function App() {
       .then((json) => setUsers(json));
   }, []);
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const result = await axios(
+        `https://5f9a10979d94640016f705ed.mockapi.io/api/vi/users?search=${query}`
+      );
+      setUsers(result.data);
+    };
+
+    fetchUsers();
+  }, [query]);
+
   return (
     <Router>
       <div className="App">
         <Header />
+        <Search getQuery={(q) => setQuery(q)} />
         <Switch>
           <Route exact path="/">
             <List users={users} />
